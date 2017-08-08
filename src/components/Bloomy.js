b803f2cdd889968ed07bf7db8e344df15f1ac5cd
David Noble <davidnoble37@gmail.com>
2017-08-08 14:18:54 -0700

    wine page

--- src/components/Bloomy.js
+++ src/components/Bloomy.js
@@ -21,7 +21,7 @@ const Bloomy = (props) => {
                 <img src={camembert} alt='Camembert' />
                 <p>A soft, creamy, surface-ripened cow's milk cheese. It was first made in the late 18th century at Camembert, Normandy, in northern France.</p>
                 <h5>Pairs well with: </h5>
-                <NavLink to='/'> Champagne </NavLink>
+                <NavLink to='/wine'> Champagne </NavLink>
               </div>
             </Segment>
           </Grid.Column>
@@ -33,7 +33,7 @@ const Bloomy = (props) => {
                 <img src={brie} alt='Brie' />
                 <p>A soft cow's-milk cheese named after Brie, the French region from which it originated. The rind is typically eaten, with its flavor depending largely upon the ingredients used and its manufacturing environment.</p>
                 <h5>Pairs well with: </h5>
-                <NavLink to='/'> Chardonnay </NavLink>
+                <NavLink to='/wine'> Chardonnay </NavLink>
               </div>
             </Segment>
           </Grid.Column>
@@ -45,7 +45,7 @@ const Bloomy = (props) => {
                 <img src={robiola} alt='Robiola' />
                 <p>An Italian soft-ripened cheese of the Stracchino family. It is from the Langhe region and made with varying proportions of cow’s, goat’s milk and sheep milk.</p>
                 <h5>Pairs well with: </h5>
-                <NavLink to='/'> Sparkling Wine </NavLink>
+                <NavLink to='/wine'> Sparkling Wine </NavLink>
               </div>
             </Segment>
           </Grid.Column>
@@ -57,7 +57,7 @@ const Bloomy = (props) => {
                 <img src={taleggio} alt='Taleggio' />
                 <p>A semisoft, washed-rind, smear-ripened Italian cheese that is named after Val Taleggio. The cheese has a thin crust and a strong aroma, but its flavor is comparatively mild with an unusual fruity tang.</p>
                 <h5>Pairs well with: </h5>
-                <NavLink to='/'> Pinot Blanc </NavLink>
+                <NavLink to='/wine'> Pinot Blanc </NavLink>
               </div>
             </Segment>
           </Grid.Column>
